f88ce60b7caf8e2cb7e935e6c74a19e279b27415
Rogelio Llerena <rogelio.llerena@ceiba.com.co>
2022-01-05 16:13:46 -0500

    Agregando 'authContext', 'authReducer'

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../auth/authContext';
 
 export const Navbar = () => {
     const navigate = useNavigate();
+
+    const { user } = useContext(AuthContext);
 
     const handleLogout = () =>{
         navigate('/login',{
             replace: true
         })
-        console.log('logout');
     }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -47,7 +49,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-info'>Rogelio</span>
+                    <span className='nav-item nav-link text-info'>{user.name}</span>
                     <button 
                         className="nav-item nav-link btn" 
                         onClick={handleLogout}
