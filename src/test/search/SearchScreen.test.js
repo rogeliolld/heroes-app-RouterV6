27bf112906527eecd685accccdf1ce469e10e3e3
Rogelio Llerena <rogelio.llerenad@gmail.com>
2022-01-06 21:03:40 -0500

    Inicio del Test 'SearchScreen'

---
+++ src/test/search/SearchScreen.test.js
@@ -0,0 +1,48 @@
+import { mount } from "enzyme"
+import { MemoryRouter } from "react-router-dom";
+import { SearchScreen } from "../../components/search/SearchScreen"
+
+describe('Pruebas en <SearchScreen/>', () => {
+    
+    test('debe de mostrarse correctamente con valores por defecto', () => {
+        
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.alert-info').text().trim()).toBe('Buscar un héroe');
+        
+
+    });
+
+    test('debe de mostrar a Batman y el input con el valor del queryString', () => {
+       
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('input').prop('value')).toBe('batman');
+        expect(wrapper).toMatchSnapshot();
+        
+    });
+
+    test('debe de mostrar a un error si no se encuentra el Hero', () => {
+       
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchScreen/>
+            </MemoryRouter>
+        );
+
+        expect(wrapper.find('.alert-danger').text().trim()).toBe('No hay resultados: batman123');
+        
+    });
+    
+    
+
+})
